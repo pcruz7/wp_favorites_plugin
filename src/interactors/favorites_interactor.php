e8aa2885d0e214ab52c46b9eaf863822c711e4ed
pcruz <pedro.cruz@log.pt>
2014-05-13 18:08:13 +0100

    Add favorites interactor

---
+++ src/interactors/favorites_interactor.php
@@ -0,0 +1,150 @@
+<?php
+
+if (!class_exists('FavoritesInteractor'))
+{
+  class FavoritesInteractor
+  {
+    private $repository;
+    private $listname;
+
+    function __construct($repository, $listname)
+    {
+      $this->repository = $repository;
+      $this->listname   = $listname;
+    }
+
+    public function isFavorite($request)
+    {
+      $userId    = $this->currentUser();
+      $favorites = $this->repository->getFavoriteList($this->listname, $userId );
+      $index     = array_search((int) $request['post_id'], $favorites);
+      return $this->indexExists($index);
+    }
+
+    public function toggleFavorite($request)
+    {
+      $userId    = $this->currentUser();
+      $favorites = $this->repository->getFavoriteList($this->listname, $userId );
+      $index     = array_search((int) $request['post_id'], $favorites);
+      $this->indexExists($index) ? $this->removeFromFavorites($userId , $favorites, $index)
+                                 : $this->addToFavorites($userId , $favorites, (int) $request['post_id']);
+      return ($this->indexExists($index) ? false : (int) $request['post_id']);
+    }
+
+    public function favoriteList($request)
+    {
+      $userId    = $this->currentUser();
+      $favorites = $this->repository->getFavoriteList($this->listname, $userId);
+      return (empty($favorites) ? array()
+                                : $this->getFavorites($favorites, $request));
+    }
+
+    public function getFavoriteById($request)
+    {
+      $args = array(
+        'p'           => $request['post_id'],
+        'post_type'   => get_post_types(array('public' => true)),
+        'post_status' => 'publish');
+      $query = new WP_Query($args);
+      return $query->have_posts() ? $this->getPost($query)
+                                  : false;
+    }
+
+    public function initList(WP_User $user = null)
+    {
+      $this->repository->initFavoriteList($this->listname, $user->ID);
+    }
+
+    private function currentUser()
+    {
+      $user = wp_get_current_user();
+      return is_user_logged_in() ? $user->ID : null;
+    }
+
+    private function indexExists($index)
+    {
+      return !($index === false or !isset($index));
+    }
+
+    private function getPost($query)
+    {
+      $query->the_post();
+      return new PostData(get_the_id(), get_the_title(), get_the_permalink());
+    }
+
+    private function getFavorites($favorites, $request)
+    {
+      extract($request);
+      $paged          = (int) $paged;
+      $posts_per_page = (int) $posts_per_page;
+      $q = array(
+        'post_type'      => get_post_types(array('public' => true)),
+        'post__in'       => $favorites,
+        'paged'          => $paged,
+        'posts_per_page' => $posts_per_page,
+        'post_status'    => 'publish',
+        'order'          => $order,
+        'orderby'        => $orderby
+      );
+
+      $query   = new WP_Query($q);
+      $results = array();
+      while ($query->have_posts()) {
+        $query->the_post();
+        array_push($results, new PostData(get_the_ID(), get_the_title(), get_the_permalink()));
+      }
+
+      return $results;
+    }
+
+    private function removeFromFavorites($user, $favorites, $key)
+    {
+      unset($favorites[$key]);
+      $this->repository->saveFavoriteList($this->listname, $user, $favorites);
+    }
+
+    private function addToFavorites($user, $favorites, $postID)
+    {
+      array_unshift($favorites, $postID);
+      $favorites = array_unique($favorites);
+      $this->repository->saveFavoriteList($this->listname, $user, $favorites);
+    }
+  }
+}
+
+if (!class_exists('PostData'))
+{
+  class PostData
+  {
+    private $title;
+    private $permalink;
+    private $id;
+
+    function __construct($id, $title = '', $permalink = '')
+    {
+      $this->title     = $title;
+      $this->permalink = $permalink;
+      $this->id        = $id;
+    }
+
+    public function getId()
+    {
+      return $this->id;
+    }
+
+    public function getTitle()
+    {
+      return $this->title;
+    }
+
+    public function getPermalink()
+    {
+      return $this->permalink;
+    }
+
+    public function toJson()
+    {
+      return array('id' => $this->id, 'title' => $this->title, 'permalink' => $this->permalink);
+    }
+  }
+}
---
+++ tests/interactors/test_favorites_interactor.php
@@ -0,0 +1,191 @@
+<?php
+
+class TestAjaxinteractor extends WP_UnitTestCase
+{
+  private $interactor;
+  private $repository;
+  private $user;
+  private $userId;
+
+  const LISTNAME = 'favorites-list';
+
+  public function setUp()
+  {
+    parent::setUp();
+    $this->user       = $this->factory->user->create();
+    $this->repository = $this->getMock('FavoritesRepository');
+    $this->interactor = new FavoritesInteractor($this->repository, self::LISTNAME);
+  }
+
+  public function tearDown()
+  {
+    parent::tearDown();
+    unset($this->interactor);
+  }
+
+  /**
+   * interactor isFavorite()
+   */
+  public function testIsFavoriteWithFavoritedPost()
+  {
+    $this->givenUserIsLoggedIn();
+    $this->givenPostIsFavorited();
+    $response = $this->onIsFavoriteRequest();
+    $this->assertTrue($response, 'interactor should return true for favorited post');
+  }
+
+  public function testIsFavoriteWithNotFavoritedPost()
+  {
+    $this->givenUserIsLoggedIn();
+    $this->givenPostIsNotFavorited();
+    $response = $this->onIsFavoriteRequest();
+    $this->assertFalse($response, 'interactor should return false for un-favorited post');
+  }
+
+  private function onIsFavoriteRequest()
+  {
+    $request = array('post_id' => '1');
+    return $this->interactor->isFavorite($request);
+  }
+
+  /**
+   * interactor toggleFavorite()
+   */
+  public function testToggleFavoriteWithNotFavoritedPost()
+  {
+    $this->givenUserIsLoggedIn();
+    $this->givenPostIsNotFavorited();
+
+    $this->repository->expects($this->once())
+                     ->method('saveFavoriteList')
+                     ->with(self::LISTNAME, $this->userId, array(1, 4, 5, 6));
+    $this->assertEquals($this->requestToToggleFavorite(), 1, 'should return the post id when toggling not favorited post');
+  }
+
+  public function testToggleFavoriteWithFavoritedPost()
+  {
+    $this->givenUserIsLoggedIn();
+    $this->givenPostIsFavorited();
+
+    $this->repository->expects($this->once())
+                     ->method('saveFavoriteList')
+                     ->with(self::LISTNAME, $this->userId, array(1 => 2, 2 => 3));
+    $this->assertFalse($this->requestToToggleFavorite(), 'should return false when toggling favorited post');
+  }
+
+  private function requestToToggleFavorite()
+  {
+    $request = array('post_id' => '1');
+    return $this->interactor->toggleFavorite($request);
+  }
+
+  /**
+   * interactor favoriteList()
+   */
+  public function testFavoriteListWithExistingPosts()
+  {
+    $this->givenUserIsLoggedIn();
+    $posts     = $this->givenPostsExist();
+    $favorites = $this->requestToListFavorites();
+    $this->assertEquals(count($posts), count($favorites), 'favorites should equal the created posts');
+  }
+
+  public function testFavoriteListWithNonExistingPosts()
+  {
+    $this->givenUserIsLoggedIn();
+    $posts     = $this->givenPostsDoNotExist();
+    $favorites = $this->requestToListFavorites();
+    $this->assertEquals(count($posts), 0, 'favorites should be empty');
+  }
+
+  private function requestToListFavorites()
+  {
+    $request = array(
+            'paged'          => 1,
+            'posts_per_page' => 5,
+            'order'          => 'ASC',
+            'orderby'        => 'post__in');
+    return $this->interactor->favoriteList($request);
+  }
+
+  /**
+   * interactor getFavoriteById()
+   */
+  public function testGetFavoriteByIdWithExistingPost()
+  {
+    $this->givenUserIsLoggedIn();
+    $posts = $this->givenPostsExist();
+    $post  = $this->requestToGetPostById($posts[0]);
+    $this->assertEquals(1, count($post), 'should only return one result');
+    $this->assertEquals($posts[0], $post->getId(), 'fetched post should equal the created one');
+  }
+
+  public function testGetFavoriteByIdWithNonExistingPost()
+  {
+    $this->givenUserIsLoggedIn();
+    $this->givenPostsDoNotExist();
+    $post = $this->requestToGetPostById('1');
+    $this->assertFalse($post, 'should return false for non existing post');
+  }
+
+  private function requestToGetPostById($id)
+  {
+    $request = array('post_id' => $id);
+    return $this->interactor->getFavoriteById($request);
+  }
+
+  private function givenPostsDoNotExist()
+  {
+    $posts = array();
+    $this->stubRepository($posts);
+    return $posts;
+  }
+
+  private function givenPostsExist()
+  {
+    $posts = $this->factory->post->create_many(2);
+    $this->stubRepository($posts);
+    return $posts;
+  }
+
+  /**
+   * Favorites list state
+   */
+  private function givenPostIsFavorited()
+  {
+    $return = array_map('intval', array('1', '2', '3'));
+    $this->stubRepository($return);
+  }
+
+  private function givenPostIsNotFavorited()
+  {
+    $return = array_map('intval', array('4', '5', '6'));
+    $this->stubRepository($return);
+  }
+
+  private function stubRepository($array)
+  {
+    $this->repository->expects($this->any())
+                     ->method('getFavoriteList')
+                     ->with(self::LISTNAME, $this->userId)
+                     ->will($this->returnValue($array));
+  }
+
+  /**
+   * User logged state
+   */
+  private function givenUserIsLoggedIn()
+  {
+    $this->userLogged($this->user);
+  }
+  private function givenUserIsLoggedOut()
+  {
+    $this->userLogged(null);
+  }
+
+  private function userLogged($user)
+  {
+    $this->userId = $user;
+    wp_set_current_user($this->userId);
+  }
+}
